7df16cd7dc439d69e047da71e2ece80302cd2835
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2025-11-16 21:46:53 +0300

    lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    # grant access when username is "admin" (case-insensitive) and password is "12345"
+    if isinstance(username, str) and username.lower() == "admin" and password == "12345":
+        return "Access granted"
+    return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    # return different messages depending on the temperature
+    if temperature < 40:
+        return "It's brisk out there!"
+    if temperature < 65:
+        return "It's a little chilly out there!"
+    if 65 <= temperature <= 85:
+        return "It's perfect out there!"
+    return "It's too dang hot out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    if num % 3 == 0:
+        return "Fizz"
+    if num % 5 == 0:
+        return "Buzz"
+    return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    if operation == "-":
+        return num1 - num2
+    if operation == "*":
+        return num1 * num2
+    if operation == "/":
+        return num1 / num2
+    print("Invalid operation!")
+    return None
