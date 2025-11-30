29d0939b7a2a7389a4607811206347945a1c002a
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2025-11-30 14:39:13 +0300

    lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,38 +1,41 @@
-#!/usr/bin/env python3
-
 def admin_login(username, password):
-    # grant access when username is "admin" (case-insensitive) and password is "12345"
-    if isinstance(username, str) and username.lower() == "admin" and password == "12345":
+    if (username == "admin" or username == "ADMIN") and password == "12345":
         return "Access granted"
-    return "Access denied"
+    else:
+        return "Access denied"
+
 
 def hows_the_weather(temperature):
-    # return different messages depending on the temperature
     if temperature < 40:
         return "It's brisk out there!"
-    if temperature < 65:
+    elif 40 <= temperature <= 65:
         return "It's a little chilly out there!"
-    if 65 <= temperature <= 85:
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
         return "It's perfect out there!"
-    return "It's too dang hot out there!"
+
 
 def fizzbuzz(num):
     if num % 3 == 0 and num % 5 == 0:
         return "FizzBuzz"
-    if num % 3 == 0:
+    elif num % 3 == 0:
         return "Fizz"
-    if num % 5 == 0:
+    elif num % 5 == 0:
         return "Buzz"
-    return num
+    else:
+        return num
+
 
 def calculator(operation, num1, num2):
     if operation == "+":
         return num1 + num2
-    if operation == "-":
+    elif operation == "-":
         return num1 - num2
-    if operation == "*":
+    elif operation == "*":
         return num1 * num2
-    if operation == "/":
+    elif operation == "/":
         return num1 / num2
-    print("Invalid operation!")
-    return None
+    else:
+        print("Invalid operation!")
+        return None
